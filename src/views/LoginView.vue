af44c17ad0fa5b9ca111b0b93be846542aec87ff
Bruna Campos <92brunacampos@gmail.com>
2022-03-18 17:31:52 -0300

    Login Page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,64 @@
 <template>
-	<h1>Oi Rota Login</h1>
+  <v-form v-model="valid">
+      <v-row>
+				<v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            :counter="10"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-col>
+
+      </v-row>
+		<v-btn
+			class="btn-login"
+			color="green"
+			type="submit"
+			elevation="2"
+			to="/home"
+		>Login</v-btn>
+	</v-form>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data: () => ({
+      valid: false,
+      firstname: '',
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length <= 10 || 'Password must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+    }),
+  }
 </script>
 
-<style>
+<style scoped>
+	.btn-login {
+		color: white;
+	}
 
 </style>
