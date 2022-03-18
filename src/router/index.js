0052353e975dff273771ac8fbfc7944298524ddd
Bruna Campos <92brunacampos@gmail.com>
2022-03-18 19:51:52 -0300

    rota home e arquivo de data(filmes)

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-//import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  //{
-    //path: '/',
-    //name: 'home',
-    //component: HomeView
-  //},
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView
+  },
   {
     path: '/',
     name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: LoginView
   }
 ]
